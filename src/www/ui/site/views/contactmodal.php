418b3aed4ed3dd0f5fe2fbbcd6f91afa4c8d00f4
sammme <samyak.upadhyay123@gmail.com>
2016-02-02 14:23:40 +0530

    created separate files for login and contact modal

---
+++ src/www/ui/site/views/contactmodal.php
@@ -0,0 +1,51 @@
+<div id="contactModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+            <div class="ui-105">
+                <!-- Combined Form Content -->
+                <div class="ui-105-content">
+                    <ul class="nav nav-tabs nav-justified">
+                      <li class="link-three"><a href="#contact-block" data-toggle="tab"><i class="fa fa-envelope"></i>Contact</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane fade" id="contact-block">
+                            <!-- Contact Block Form -->
+                            <div class="contact-block-form">
+                                <h4>Contact Form</h4>
+                                <!-- Border -->
+                                <div class="bor bg-purple"></div>
+                                <!-- Form -->
+                                <form class="form" role="form">
+                                    <!-- Form Group -->
+                                    <div class="form-group">
+                                        <label class="control-label">Name</label>
+                                        <input type="text" class="form-control" placeholder="Enter Name">
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label">Email</label>
+                                        <input type="text" class="form-control" placeholder="Enter Email">
+                                    </div>
+                                     <div class="form-group">
+                                        <label for="comments" class="control-label">Comments</label>
+                                        <textarea class="form-control" id="comments" rows="5" placeholder="Enter Comments"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <!-- Buton -->
+                                        <button type="submit" class="btn btn-red">Submit</button>&nbsp;
+                                        <button type="submit" class="btn btn-lblue">Reset</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!--div ui-105-->
+        </div><!-- Modal body-->
+    </div><!-- Modal content-->
+    </div><!-- Modal dialog-->
+    </div>
---
+++ src/www/ui/site/views/ourteam.php
@@ -0,0 +1,245 @@
+
+<div class="row">
+   <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false">
+         <!-- Indicators -->
+         <ol class="carousel-indicators">
+           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+           <li data-target="#myCarousel" data-slide-to="1"></li>
+           <li data-target="#myCarousel" data-slide-to="2"></li>
+           <li data-target="#myCarousel" data-slide-to="3"></li>
+         </ol>
+
+         <!-- Wrapper for slides -->
+         <div class="carousel-inner ui-x" role="listbox">
+           <div class="item active">
+               <div class="carousel-caption" style="position: static;">
+                       <h3>Web Support Team</h3>
+                       <p>Web Support Team of AIJAA.</p>
+               </div>
+              <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Varun</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">Web Project manager</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <i class="fa fa-linkedin linkedin"></i>
+                                               <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+
+                   </div>
+                   <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Venkat Parsi</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">Developer</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+                   </div>
+                   <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Rammurthy</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">Multimedia Specialist</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <!-- LinkedIn -->
+                                               <i class="fa fa-linkedin linkedin"></i>
+                                               <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+                   </div>
+                   <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Padmaja</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">Site Administrator</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <!-- LinkedIn -->
+                                               <i class="fa fa-linkedin linkedin"></i>
+                                               <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+                   </div>
+
+
+           </div>
+
+           <div class="item">
+
+              <div class="carousel-caption" style="position: static;">
+                   <h3>Management Team</h3>
+                   <p>AIJAA PAN India Managment Team.</p>
+               </div>
+
+                <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Anuj Goswami</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">President</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <!-- LinkedIn -->
+                                               <i class="fa fa-linkedin linkedin"></i>
+                                                <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+
+                   </div>
+                   <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Loknath Reddy</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">Vice President</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <!-- LinkedIn -->
+                                               <i class="fa fa-linkedin linkedin"></i>
+                                               <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+                   </div>
+                   <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Ravinder</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">Treseury</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <!-- Twitter -->
+                                               <i class="fa fa-twitter twitter"></i>
+                                               <!-- LinkedIn -->
+                                               <i class="fa fa-linkedin linkedin"></i>
+                                               <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+                   </div>
+                   <div class="col-md-3 col-sm-6 col-xs-6 col-mob">
+                                   <!-- Item -->
+                                   <a href="#" class="ui-item">
+                                       <!-- User Image -->
+                                       <img src="../assets/img/default-avatar.png" alt="" class="ui-front ui-flip img-responsive" />
+                                       <!-- Flip Back -->
+                                       <span class="ui-back ui-flip">
+                                           <!-- User Name -->
+                                           <span class="ui-name">Mukesh Jaiswal</span>
+                                           <!-- Designation -->
+                                           <span class="ui-desig">Secretary</span>
+                                           <!-- Social Media -->
+                                           <span class="ui-social">
+                                               <!-- Facebook -->
+                                               <i class="fa fa-facebook facebook"></i>
+                                               <!-- LinkedIn -->
+                                               <i class="fa fa-linkedin linkedin"></i>
+                                               <!-- Google-Plus -->
+                                               <i class="fa fa-google-plus google-plus"></i>
+                                           </span>
+                                       </span>
+                                   </a>
+                   </div>
+
+           </div>
+
+           <div class="item">
+
+              <div class="carousel-caption" style="position: static;">
+                   <h3>Southeren Region Coordinators</h3>
+                   <p>AIJAA Executive body of southern india.</p>
+               </div>
+           </div>
+
+           <div class="item">
+
+              <div class="carousel-caption" style="position: static;">
+                   <h3>Northern Region Coordinators</h3>
+               <p>AIJAA Executive body of Northern india.</p>
+               </div>
+           </div>
+   </div>
+
+<!-- Left and right controls -->
+<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+<span class="sr-only">Previous</span>
+</a>
+<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+<span class="sr-only">Next</span>
+</a>
+</div>
+</div>
